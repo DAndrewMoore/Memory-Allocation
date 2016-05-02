4cbb9ad178b2149514528cace37d7e61d2e2d76f
DAndrewMoore <d.andrew.moo@gmail.com>
2016-05-02 09:45:18 -0500

    Update to FIFO method which implements most (if not all) memory management stipulations. Refactoring is still a possibility.

--- project2.cpp
+++ project2.cpp
@@ -18,7 +18,7 @@ int* memoryScope(){
     return space;
 }
 
-int checkMemory(int* space, int memorySize){
+int checkMemory(int& space, int memorySize){
     int openSize = 0;
     int i=1;
     
@@ -34,7 +34,7 @@ int checkMemory(int* space, int memorySize){
         return 0;
 }
 
-int my_malloc(int* space, int memoryReq){
+int my_malloc(int& space, int memoryReq){
     int holeStart = checkMemory(memoryReq);
     
     if(holeStart){
@@ -45,26 +45,47 @@ int my_malloc(int* space, int memoryReq){
     return holeStart;
 }
 
-void my_free(int memoryLocale, int memorySize, int* space){
+void my_malloc(int& space, int memoryReq, int holeStart){
+    for(int i=0; i<memoryReq; i++)
+        space[holeStart+i] = 1;
+}
+
+void my_free(int& space, int memorySize, int memoryLocale){
     for(int i=0; i<memorySize; i++)
         space[memoryLocale + i] = 0;
 }
 
-void FIFO(vector<processStruct> pVec, int processors = 1){
+void FIFO(vector<processStruct> pVec, int processors = 4){
     vector<processStruct> waitQueue;
     vector<processStruct> executing;
     int waitTime = 0;
+    int checkMem = 1;
     int* space = memoryScope();
      
     while(1){
         //If we need to enqueue a new process
         if(waitTime % 50 == 0 && pVec.size() != 0){
             //If we should put it straight on the processor
-            if(executing.size() == 0 && executing.size() < processors){
-                processStruct pS = pVec.front(); //get the process
-                int holeStart = my_malloc(space, pS.memoryPrint); //unconditional malloc since 0 processes executing
-                pS.memoryOffset = holeStart; //assign the holeStart position for faster deletion
-                executing.push_back(pS); //push on to executing block
+            if(executing.size() < processors){
+                //If we have more than one processor in execution
+                if(executing.size() != 0){
+                    processStruct pS = pVec.front(); //Get the process
+                    int holeStart = checkMemory(space, pS.memoryPrint); //Check if we can add it to the execution list
+                    
+                    //If we can add it to the execution list
+                    if(holeStart > 0){
+                        pS.memoryOffset = holeStart; //set the offset
+                        my_malloc(space, pS.memoryPrint, holeStart); //assign the space
+                        executing.push_back(pS); //add it to execution list
+                    } else {
+                        waitQueue.push_back(pS); //If we can't, then put it on the waitQueue
+                    }
+                } else { //If we have no processors in use
+                    processStruct pS = pVec.front(); //get the process
+                    int holeStart = my_malloc(space, pS.memoryPrint); //unconditional malloc since 0 processes executing
+                    pS.memoryOffset = holeStart; //assign the holeStart position for faster deletion
+                    executing.push_back(pS); //push on to executing block
+                }
             }
             else //otherwise push on to waitqueue
                 waitQueue.push_back(pVec.front());
@@ -74,11 +95,19 @@ void FIFO(vector<processStruct> pVec, int processors = 1){
         }
         
         //If we're not at process income point, but we need something to process
-        if(executing.size() == 0 && waitQueue.size() > 0){
+        if(checkMem && executing.size() < processors && waitQueue.size() > 0){
             processStruct pS = waitQueue.front(); //Get the process at front of waiting queue
-            pS.memoryOffset = my_malloc(space, pS.memoryPrint); //get the memory start position
-            executing.push_back(pS); //push on to executing block
-            waitQueue.erase(waitQueue.begin()); //delete from waiting queue
+            int holeStart = checkMemory(space, pS.memoryPrint); //Check if we have enough memory available
+            
+            //If we have enough memory
+            if(holeStart > 0){
+                pS.memoryOffset = holeStart; //Set the memory start position
+                my_malloc(space, pS.memoryPrint, holeStart); //Assign the space
+                executing.push_back(pS); //push on to executing block
+                waitQueue.erase(waitQueue.begin()); //delete from waiting queue
+            } else { //If we don't have enough memory
+                checkMem = 0; //Don't check again until this bit is set (like if a process finishes execution)
+            }
         }
         
         //If we have processes in execution
@@ -90,11 +119,13 @@ void FIFO(vector<processStruct> pVec, int processors = 1){
         //Check if any process is done
         for(int i=0; i<executing.size(); i++)
             if(executing[i].cycleCount <= 0){
-                processStruct pS = executing[i];
-                my_free(pS.memoryOffset, pS.memoryPrint, space);
-                executing.erase(executing.begin());
+                processStruct pS = executing[i]; //get the process info
+                my_free(pS.memoryOffset, pS.memoryPrint, space); //free the space
+                executing.erase(executing.begin()); //erase the process
+                checkMem = 1; //initiate a check if waitQueue processes are waiting for memory
             }
         
+        //Increase waitTime by amount of processes waiting
         waitTime += waitQueue.size();
         
         if(pVec.size() == 0 && executing.size() == 0 && waitQueue.size() == 0)
